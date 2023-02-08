6c42209191b98ac47437831286fd03bfe6af9361
matteolutz <matteo.lutz06@outlook.de>
2023-02-08 12:39:37 +0100

    changes

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import vlc
 import pathlib
 
+MEDIA_DIR = pathlib.Path(__file__).parent / "rsc/default"
+RFID_DIR = pathlib.Path(__file__).parent / "rsc/rfid"
+
+
+def get_rfid():
+    # return rfid id if rfid is detected, otherwise return None
+    # TODO: implement this
+    return None
+
 
 def main():
-    # Create a new instance of vlc media player
-    player = vlc.MediaPlayer()
-    player.toggle_fullscreen()
+    default_media = [x for x in MEDIA_DIR.glob("*") if x.is_file()]
+    current_media_playing = 0
 
-    media = vlc.Media(pathlib.Path(__file__).parent / "rsc/test.mp4")
-    player.set_media(media)
+    player = vlc.MediaPlayer()
+    player.set_fullscreen(True)
+
+    player.set_media(vlc.Media(default_media[current_media_playing]))
     player.play()
 
-    start_time = player.get_time()
-
     while True:
-        pass
+        rfid = get_rfid()
+        if rfid is not None:
+            print(f"RFID detected: {rfid}")
+            media = RFID_DIR / f"{rfid}.mp4"
+            if media.exists():
+                current_media_playing = 0
+                player.set_media(vlc.Media(media))
+                player.play()
+                continue
+
+        if player.get_state() == vlc.State.Ended:
+            current_media_playing += 1
+            if current_media_playing >= len(default_media):
+                current_media_playing = 0
+            player.set_media(vlc.Media(default_media[current_media_playing]))
+            player.play()
 
 
 if __name__ == '__main__':
